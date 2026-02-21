a2a9f9c94e9843c6999318c77b836632c1bab752
SATYAM SHAH <satyam.s8092@gmail.com>
2026-02-21 19:01:15 +0530

    basic Style

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const mongoose = require('mongoose')
+const ejsMate=require('ejs-mate')
 const app = express()
 const Campground = require('./models/campground')
 const methodOverride = require('method-override')
@@ -14,14 +15,15 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.engine('ejs',ejsMate)
 
 app.get('/', (req, res) => {
     res.render('home')
 })
 
 app.get('/campgrounds', async (req, res) => {
-    const campground = await Campground.find({})
-    res.render('campground/index', { campground })
+    const campgrounds = await Campground.find({})
+    res.render('campground/index', { campgrounds })
 })
 
 app.get('/campgrounds/new', (req, res) => {
--- seeds/index.js
+++ seeds/index.js
@@ -12,16 +12,20 @@ const sample = array => array[Math.floor(Math.random() * array.length)]
 
 const seedDb = async () => {
     await Campground.deleteMany({})
+    let rand = Math.floor(Math.random() * 3000) + 10
     for (let i = 0; i < 50; i++) {
         const random = Math.floor(Math.random() * 1000)
         const c = new Campground({
             location: `${cities[random].city}, ${cities[random].state}`,
-            title:`${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: "https://picsum.photos/200",
+            description: 'Good availability and great rates for a place to camp. Read property reviews and choose the best deal for your stay.',
+            price: rand
         })
         await c.save()
     }
 }
 
-seedDb().then(()=>{
+seedDb().then(() => {
     mongoose.connection.close()
 })
